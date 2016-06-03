80bdcb3e6a0af9c44c24d0fb6299920dab2b22ef
chick <chick@qrhino.com>
2016-06-02 22:36:52 -0700

    change to use iotesters instead of hwiotesters change dependencies to release snapshots as of 5/18/2016

--- src/test/scala/examples/test/GCDUnitTest.scala
+++ src/test/scala/examples/test/GCDUnitTest.scala
@@ -7,6 +7,13 @@ import Chisel.iotesters.{ChiselFlatSpec, SteppedHWIOTester}
 import example.GCD
 
 class GCDUnitTester extends SteppedHWIOTester {
+  /**
+    * compute the gcd and the number of steps it should take to do it
+    *
+    * @param a positive integer
+    * @param b positive integer
+    * @return the GCD of a and b
+    */
   def computeGcd(a: Int, b: Int): (Int, Int) = {
     var x = a
     var y = b
@@ -23,25 +30,29 @@ class GCDUnitTester extends SteppedHWIOTester {
     (x, depth)
   }
 
-  val (a, b, z) = (64, 48, 16)
   val device_under_test = Module(new GCD)
   val gcd = device_under_test
 
-  poke(gcd.io.a, a)
-  poke(gcd.io.b, b)
-  poke(gcd.io.e, 1)
-  step(1)
-  poke(gcd.io.e, 0)
+  for(i <- 1 to 100) {
+    for(j <- 1 to 100) {
+      val (a, b, z) = (64, 48, 16)
 
-  val (expected_gcd, steps) = computeGcd(a, b)
+      poke(gcd.io.a, a)
+      poke(gcd.io.b, b)
+      poke(gcd.io.e, 1)
+      step(1)
+      poke(gcd.io.e, 0)
 
-  step(steps - 1) // -1 is because we step(1) already to toggle the enable
-  expect(gcd.io.z, expected_gcd)
-  expect(gcd.io.v, 1)
+      val (expected_gcd, steps) = computeGcd(a, b)
+
+      step(steps - 1) // -1 is because we step(1) already to toggle the enable
+      expect(gcd.io.z, expected_gcd)
+      expect(gcd.io.v, 1)
+    }
 }
 
 class GCDTester extends ChiselFlatSpec {
-  "a" should "b" in {
+  "GCD circuit" should "compute the correct greatest common denominator" in {
     assertTesterPasses { new GCDUnitTester }
   }
 }
