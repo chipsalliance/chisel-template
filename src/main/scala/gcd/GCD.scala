82f6f6eba39841c496cef18c83299bc76e5d680c
chick <chick.markley@sifive.com>
2022-03-03 14:44:42 -0800

    Format all scala files Update deprecated chiseltest usage Update deprecated scalatest usage

--- src/main/scala/gcd/GCD.scala
+++ src/main/scala/gcd/GCD.scala
@@ -11,18 +11,17 @@ import chisel3._
   */
 class GCD extends Module {
   val io = IO(new Bundle {
-    val value1        = Input(UInt(16.W))
-    val value2        = Input(UInt(16.W))
+    val value1 = Input(UInt(16.W))
+    val value2 = Input(UInt(16.W))
     val loadingValues = Input(Bool())
-    val outputGCD     = Output(UInt(16.W))
-    val outputValid   = Output(Bool())
+    val outputGCD = Output(UInt(16.W))
+    val outputValid = Output(Bool())
   })
 
-  val x  = Reg(UInt())
-  val y  = Reg(UInt())
+  val x = Reg(UInt())
+  val y = Reg(UInt())
 
-  when(x > y) { x := x - y }
-    .otherwise { y := y - x }
+  when(x > y) { x := x - y }.otherwise { y := y - x }
 
   when(io.loadingValues) {
     x := io.value1
